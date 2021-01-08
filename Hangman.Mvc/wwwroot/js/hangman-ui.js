f000eb9a7797f2094bc02b292f3bf6abe10e5892
Matt Downs <mdowns56@gmail.com>
2021-01-08 10:36:35 -0800

    initial commit

---
+++ Hangman.Mvc/wwwroot/js/hangman-ui.js
@@ -0,0 +1,53 @@
+﻿$(function () {
+    let wins = 0;
+    let losses = 0;
+    let game = Hangman();
+    display(game.gameState());
+
+    $('.guess:not(.right,.wrong)').on('click', function () {
+        let letter = $(this).val();
+        let result = game.guess(letter);
+
+        if (result.state) {
+            display(result);
+
+            $(this).addClass(result.guesses.find(c => c.letter == letter).right ? 'right' : 'wrong');
+
+            if (game.isOver()) {
+                if (result.state == 'win')
+                    wins++;
+                else losses++;
+                endGame(game, wins, losses);
+            }
+        }
+    });
+
+    $('.restart').on('click', function () {
+        restart(game);
+        $(this).hide();
+    });
+});
+
+const display = (gameState) => {
+    let outputElements = gameState.output.map((x) => $(`<span>${x}</span>`));
+    $('.output').html(outputElements);
+
+    $('.picture').html(gameState.state);
+}
+
+const endGame = (game, wins, losses) => {
+    let result = game.gameState();
+    $('.guess').prop('disabled', 'disabled');
+    $('.restart').show();
+    $('.picture').html('');
+    $('.ending').addClass(result.state).html(result.state);
+    $('.record').html($(`<span>${wins}-${losses}</span>`));
+}
+
+const restart = (game) => {
+    game.start();
+    display(game.gameState());
+
+    $('.guess').removeClass(['right', 'wrong']).prop('disabled', false);
+    $('.ending').removeClass(['win','loss']).html('');
+}
---
+++ Hangman.Mvc/wwwroot/js/hangman.js
@@ -0,0 +1,126 @@
+﻿const Hangman = () => {
+
+    let secretWord;
+    let numMissedGuessToLose;
+    let guesses = [];
+    let state;
+    let maskedWord = [];
+    
+    const words = ['apple', 'banana', 'cherry', 'pineapple', 'strawberry', 'melon'];
+
+    const start = () => {
+        let random = Math.floor(Math.random() * words.length);
+
+        init(words[random]);
+
+        return gameState();
+    }
+
+    const init = (word) => {
+        secretWord = word;
+        maskedWord = [];
+        for (i = 0; i < secretWord.length; i++)
+            maskedWord.push(false);
+        numMissedGuessToLose = 6;
+        guesses = [];
+        setState();
+    }
+
+    const guess = (letter) => {
+        if (gameStillOn()) {
+            let validationResult = validateLetter(letter);
+
+            if (!validationResult.isValid)
+                return validationResult;
+
+            
+            let found = placeLetter(letter);
+
+            if (!found) {
+                numMissedGuessToLose--;
+            }
+
+            guesses.push({ 'letter': letter, 'right': found });
+
+            setState();
+        }
+
+        return gameState();
+    }
+
+    const placeLetter = (letter) => {
+        let found = false;
+
+        for (i = 0; i < secretWord.length; i++) {
+            if (secretWord[i] == letter) {
+                maskedWord[i] = letter;
+                found = true;
+            }
+        }
+
+        return found;
+    }
+
+    const gameState = () => {
+        return {
+            'state': state,
+            'output': output(),
+            'guesses': guessedLetters()
+        };
+    }
+
+    const setState = () => {
+        if (checkLoss())
+            state = 'loss';
+        else if (checkWin())
+            state = 'win';
+        else
+            state = numMissedGuessToLose.toString();
+    }
+
+    const gameStillOn = () => state != 'loss' && state != 'win';
+
+    const isOver = () => !gameStillOn();
+
+    const checkWin = () => {
+        for (i = 0; i < maskedWord.length; i++)
+            if (maskedWord[i] == false)
+                return false;
+
+        return true;
+    }
+
+    const checkLoss = () => {
+        return numMissedGuessToLose == 0;
+    }
+
+    const guessedLetters = () => guesses.slice();
+
+    const validateLetter = (c) => {
+        if (!c || c.length != 1)
+            return { isValid: false, message: 'Invalid letter' };
+
+        c = c.toLowerCase();
+
+        if (c < 'a' || c > 'z')
+            return { isValid: false, message: 'Letter must be a-z' };
+
+        if (guesses.find(g => g.letter == c))
+            return { isValid: false, message: 'Letter already guessed' };
+
+        return { isValid: true };
+    }
+
+    const output = () => {
+        return maskedWord.map( v => !v ? '_' : v );
+    }
+
+    start();
+
+    return {
+        guess,
+        gameState,
+        isOver,
+        start
+    }
+}
